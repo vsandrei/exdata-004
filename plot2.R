5df02619435e1cf40990f9d41ea7b21e65f9f6f6
vsandrei <vandrei@gmu.edu>
2014-07-12 11:44:12 -0700

    Plot 2

---
+++ plot2.R
@@ -0,0 +1,34 @@
+# plot2.R
+# exdata-004 Assignment 1
+# 07.12.2014
+# vandrei@gmu.edu
+
+# read the data file for 02/01/2007 and 02/02/2007 only
+data <- read.table("household_power_consumption.txt", 
+                   header = TRUE, sep = ";",
+                   skip = 66636, nrows = 2880)
+
+colnames(data) <- c("Date", "Time",
+                    "Global_active_power",
+                    "Global_reactive_power",
+                    "Voltage",
+                    "Global_intensity",
+                    "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+
+# date and time conversions
+x <- strptime(paste(as.Date(data$Date, format = "%d/%m/%Y"), data$Time),
+              format = "%Y-%m-%d %H:%M:%S")
+
+# open the graphics device
+png(filename = "plot2.png",
+    width = 480, height = 480, units = "px",
+    bg = "white")
+
+# plot the line graph
+plot(x, data$Global_active_power, 
+     type = "l", 
+     xlab = "",
+     ylab = "Global Active Power (kilowatts)")
+
+# close the graphics device
+dev.off()
